94b92cd395264adbfb2adfdad69f634284e6666a
TANHEHANZ <hantach10@gmail.com>
2025-09-19 10:46:06 -0400

    fix(bd):correccion de decoradores

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,11 +2,12 @@ import dotenv from "dotenv";
 dotenv.config();
 import { createServer } from "./server";
 import config from "./infraestructure/config/config";
+import { InitBD } from "./infraestructure/config/database";
 
 const startServer = async () => {
   try {
     const server = createServer();
-
+    await InitBD();
     server.listen(config.port, () => {
       console.log(` Running on port: http://localhost:${config.port}`);
     });
